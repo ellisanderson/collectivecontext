4d581dbe0b39c68ae728df2bc2741c92f8ac0d07
ellisanderson <ellishenryanderson@gmail.com>
2017-09-14 21:49:25 -0400

    added instructions page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+  var header = "<header><div class='logo'><a href='index.html'>&rarr;CC&larr;</a></div><nav><ul class='main-nav'><li><a href='tunnel.html'>Tunnel</a></li></ul><div class='ham-trigger'>Menu</div><div class='ham-nav'><ul><li><a href='tunnel.html'>Tunnel</a></li></ul></div></nav></header>";
+  var footer = "<footer><a href='instructions.html'>Want to participate?</a></footer>";
+
+  $('body').prepend(header);
+  $('body').append(footer);
 
   $('.ham-trigger, .ham-nav').click(function(){
     $('.ham-nav').fadeToggle();
